9fc36bac48a0cca56415b83a02ec22813df81b29
RyzenMu <muruga13in2022@gmail.com>
2024-04-01 14:55:28 +0530

    println

---
+++ The_Conquest/src/Hello_World.java
@@ -0,0 +1,7 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World from class");
+        String name = "Muruganantham";
+        System.out.println("My name is :" + name);
+    }
+}

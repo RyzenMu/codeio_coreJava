b3ff11d4d93e1c801ff51327b7590173a74a060b
RyzenMu <muruga13in2022@gmail.com>
2024-04-08 00:47:43 +0530

    data types

--- The_Conquest/src/Varables_Datatypes.java
+++ The_Conquest/src/Varables_Datatypes.java
@@ -1,5 +1,15 @@
 public class Varables_Datatypes {
     public static void main(String[] args) {
-        
+        int age = 18;
+        System.out.println(age);
+        byte age1 = 33; // byte has range between -128 to 128
+        short age3 = 8763; // short -32k to 32k
+        long age4 = 123456789l; // long has value of above 20 billion
+        System.out.println(age1);
+
+        //float
+        float abc = 1.456f; // float requires f at the end
+        //double 
+        double qwe = 1.243534; //
     }
 }
